8a86536cd4970b375613bea5c67b24512481353a
sufiyan.ansari <sufiyan.ansari@visiblealpha.com>
2019-10-11 16:20:19 +0530

    Deleted package lock and dist removed added in gitignore

--- dist/ngx-material-drawer/lib/app/utils/breakpoint-observer.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { Observable } from 'rxjs';
-export declare const POINTS_NAME: string[];
-export declare class BreakpointObserverService {
-    private _size$;
-    constructor();
-    readonly size$: Observable<string>;
-    private _getScreenSize;
-}
